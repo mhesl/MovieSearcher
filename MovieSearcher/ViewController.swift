e6c734f921864ec482f91f2d26f05890b6e525cf
mohamadeslami <mohammadeslami1379@yahoo.com>
2021-04-24 10:55:29 +0430

    Initial Commit

---
+++ MovieSearcher/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieSearcher
+//
+//  Created by Mohamad Eslami on 2/4/1400 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

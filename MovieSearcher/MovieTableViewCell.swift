83f56e9b0201c7c38341e22f141ceae94367772b
mohamadeslami <mohammadeslami1379@yahoo.com>
2021-04-24 16:21:35 +0430

    fetching movies from api by title implemented

---
+++ MovieSearcher/MovieTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieTableViewCell.swift
+//  MovieSearcher
+//
+//  Created by Mohamad Eslami on 2/4/1400 AP.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- MovieSearcher/ViewController.swift
+++ MovieSearcher/ViewController.swift
@@ -7,13 +7,104 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate{
+    
+    
+    
+    @IBOutlet var tableView : UITableView!
+    @IBOutlet var textField : UITextField!
 
+    var movies = [Movie]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        textField.delegate = self
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchMovies()
+        return true
+    }
+    
+    private func searchMovies(){
+        textField.resignFirstResponder()
+        
+        guard let text = textField.text, !text.isEmpty else {
+            return
+        }
+        
+        self.movies.removeAll()
+        
+        URLSession.shared.dataTask(with: URL(string: "https://www.omdbapi.com/?apikey=3ef10bdd&s=\(text)&type=movie")! , completionHandler: {data, response, error in
+            
+            guard let data = data, error == nil else {
+                return
+            }
+            var result : MovieResult?
+            
+            do {
+                result = try JSONDecoder().decode(MovieResult.self, from: data)
+                
+            } catch {
+                print("An error occurred")
+            }
+            
+            guard let finalResult = result else {
+                return
+            }
+            
+            
+            let resultMovies = finalResult.Search
+            self.movies.append(contentsOf: resultMovies)
+            
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+            
+        }).resume()
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 
 
+}
+
+struct MovieResult: Codable {
+    let Search: [Movie]
+}
+
+
+struct Movie: Codable {
+    
+    let Title: String
+    
+    let Year:   String
+    
+    let imdbID: String
+    
+    let _Type:   String
+    
+    let Poster : String
+    
+    private enum CodingKeys: String, CodingKey {
+        case Title, Year, imdbID, _Type = "Type", Poster
+    }
+    
 }
 
